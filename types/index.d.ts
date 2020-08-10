ba69fb86f662fd01ed304c47c26860cc904eff77
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2020-08-10 10:54:41 -0700

    Remove types from vcs

--- types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import {PluginOptions} from './types';
-import {Model} from './model';
-
-export declare function generateDatabasePlugin<T>(options: PluginOptions<T>): (store: any) => void;
-
-export {Model};
--- types/relationships.d.ts
+++
@@ -1,9 +0,0 @@
-import {Relationship} from './types';
-import {Model} from './model';
-
-export declare const isList: <T extends Relationship>(definition: any) => definition is T[];
-export declare const isItem: (definition: Relationship) => Boolean;
-
-export declare function relations(relatives: any): any;
-
-export declare function getRelationshipSchema(relationship: Relationship): typeof Model;
