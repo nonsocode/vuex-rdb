c481b70b9b8ca642f63756d2365a4e16b18d00c8
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2020-09-26 00:40:31 -0700

    Change Build flow

--- build/package.js
+++ build/package.js
@@ -2,7 +2,6 @@ import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 import commonjs from '@rollup/plugin-commonjs';
-
 const commonConfig = {
   input: 'src/index.ts',
   external: ['normalizr', 'vuex', 'vue'],
