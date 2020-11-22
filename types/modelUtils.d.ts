eca1a142051a105f4c1cd7a9b05cb1424b7d9e33
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2020-11-22 00:05:01 -0800

    Husky test

--- types/modelUtils.d.ts
+++ types/modelUtils.d.ts
@@ -4,5 +4,9 @@ import { IdValue, MixedDefinition, Schema } from './types';
 import { Relationship } from './relationships/relationhsip';
 export declare function getConstructor(model: Model<any>): Schema;
 export declare function validateEntry(data: any, relationship: Relationship): boolean;
-export declare function normalizeAndStore(store: Store<any>, data: any, entityDef: MixedDefinition): IdValue | IdValue[];
+export declare function normalizeAndStore(
+  store: Store<any>,
+  data: any,
+  entityDef: MixedDefinition
+): IdValue | IdValue[];
 export declare function modelToObject(model: Model, schema: Schema, allProps: boolean, seen?: Map<Model, object>): {};
