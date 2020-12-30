e9f60214ef04d40e5adede3c8f0a07e3de79862c
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2020-12-29 18:49:27 -0800

    Fix prettier

--- types/query.d.ts
+++ types/query.d.ts
@@ -3,50 +3,50 @@ declare type WhereValue = string | number | object | any[];
 declare type WhereKey<T> = string | WhereFunction<T>;
 declare type WhereOperand = '=' | '!=' | '>' | '<' | '>=' | '<=';
 interface Where<T> {
-    key: WhereKey<T>;
-    operand?: WhereOperand;
-    value?: WhereFunction<T> | WhereValue;
+  key: WhereKey<T>;
+  operand?: WhereOperand;
+  value?: WhereFunction<T> | WhereValue;
 }
 interface WhereFunction<T> {
-    (query: Query<T>, item?: any): boolean | void;
+  (query: Query<T>, item?: any): boolean | void;
 }
 declare abstract class Query<T> {
-    protected and: Where<T>[];
-    protected or: Where<T>[];
-    constructor();
-    where(key: WhereKey<T>): this;
-    where(key: string, value: WhereValue | WhereFunction<T>): this;
-    where(key: string, operand: WhereOperand, value: WhereValue): this;
-    orWhere(key: WhereKey<T>): this;
-    orWhere(key: string, value: WhereValue | WhereFunction<T>): this;
-    orWhere(key: string, operand: WhereOperand, value: WhereValue): this;
-    private addWhere;
-    abstract get(): unknown;
-    protected matchItem(item: any): boolean;
+  protected and: Where<T>[];
+  protected or: Where<T>[];
+  constructor();
+  where(key: WhereKey<T>): this;
+  where(key: string, value: WhereValue | WhereFunction<T>): this;
+  where(key: string, operand: WhereOperand, value: WhereValue): this;
+  orWhere(key: WhereKey<T>): this;
+  orWhere(key: string, value: WhereValue | WhereFunction<T>): this;
+  orWhere(key: string, operand: WhereOperand, value: WhereValue): this;
+  private addWhere;
+  abstract get(): unknown;
+  protected matchItem(item: any): boolean;
 }
 export declare class ModelQuery<T extends Schema> extends Query<any> {
-    private schema;
-    load: Load<Schema>;
-    constructor(schema: T);
-    with(relationshipName: string): this;
-    with(relationshipName: string, queryFunction: WhereFunction<unknown>): this;
-    with(record: Record<string, WhereFunction<unknown>>): this;
-    get(): InstanceType<T>[];
+  private schema;
+  load: Load<Schema>;
+  constructor(schema: T);
+  with(relationshipName: string): this;
+  with(relationshipName: string, queryFunction: WhereFunction<unknown>): this;
+  with(record: Record<string, WhereFunction<unknown>>): this;
+  get(): InstanceType<T>[];
 }
 declare class Load<T extends Relationship = Relationship> {
-    protected relationship: T;
-    loads: Map<string, Load<Relationship>>;
-    conditions: Set<Where<T>>;
-    constructor(relationship: T);
-    addLoad(name: string, load: Load<Relationship>): void;
-    getLoad(name: any): Load<Relationship>;
-    has(name: string): boolean;
-    addCondition(where: Where<T>): void;
-    apply(data: any): RelationshipModel<T>;
-    parse(relationshipName: string, queryFunction: WhereFunction<unknown>): any;
-    parse(record: Record<string, WhereFunction<unknown> | true>): any;
-    parse(relationshipNames: string[]): any;
-    parse(relationshipName: string): any;
-    private parseRawLoadArgs;
+  protected relationship: T;
+  loads: Map<string, Load<Relationship>>;
+  conditions: Set<Where<T>>;
+  constructor(relationship: T);
+  addLoad(name: string, load: Load<Relationship>): void;
+  getLoad(name: any): Load<Relationship>;
+  has(name: string): boolean;
+  addCondition(where: Where<T>): void;
+  apply(data: any): RelationshipModel<T>;
+  parse(relationshipName: string, queryFunction: WhereFunction<unknown>): any;
+  parse(record: Record<string, WhereFunction<unknown> | true>): any;
+  parse(relationshipNames: string[]): any;
+  parse(relationshipName: string): any;
+  private parseRawLoadArgs;
 }
 export {};
--- types/query/index.d.ts
+++ types/query/index.d.ts
@@ -1,14 +1,14 @@
-import { Where, WhereFunction, WhereKey, WhereOperand, WhereValue } from "../types";
+import { Where, WhereFunction, WhereKey, WhereOperand, WhereValue } from '../types';
 export declare abstract class Query<T> {
-    protected and: Where<T>[];
-    protected or: Where<T>[];
-    protected constructor();
-    where(key: WhereKey<T>): this;
-    where(key: string, value: WhereValue | WhereFunction<T>): this;
-    where(key: string, operand: WhereOperand, value: WhereValue): this;
-    orWhere(key: WhereKey<T>): this;
-    orWhere(key: string, value: WhereValue | WhereFunction<T>): this;
-    orWhere(key: string, operand: WhereOperand, value: WhereValue): this;
-    private addWhere;
-    abstract get(): any;
+  protected and: Where<T>[];
+  protected or: Where<T>[];
+  protected constructor();
+  where(key: WhereKey<T>): this;
+  where(key: string, value: WhereValue | WhereFunction<T>): this;
+  where(key: string, operand: WhereOperand, value: WhereValue): this;
+  orWhere(key: WhereKey<T>): this;
+  orWhere(key: string, value: WhereValue | WhereFunction<T>): this;
+  orWhere(key: string, operand: WhereOperand, value: WhereValue): this;
+  private addWhere;
+  abstract get(): any;
 }
